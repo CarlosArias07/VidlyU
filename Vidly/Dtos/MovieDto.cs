ba6bac3ef6ca3512ebdcba368e375b472259e4e3
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-10-13 11:31:00 -0500

    Section 7 - Part 74

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public byte GenreId { get; set; }
+
+        [Range(1, 20)]
+        public int Stock { get; set; }
+    }
+}
