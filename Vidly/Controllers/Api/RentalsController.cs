d762999bc0f0f927187e40f0c066525560d362cb
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-10-27 11:30:32 -0500

    Section 10 - Part 116

--- Vidly/Controllers/Api/RentalsController.cs
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -22,6 +22,20 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto newRental)
         {
+
+            /*var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+
+            if (customerInDb == null)
+                return NotFound();*/
+
+            foreach (var movieId in newRental.MovieIds)
+            {
+                Rental rental = new Rental();
+                rental.CustomerId = newRental.CustomerId;
+                rental.MovieId = movieId;
+                _context.Rentals.Add(rental);
+            }
+
             return Ok();
         }
     }
